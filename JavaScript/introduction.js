982249cefb12fb1bbcec7650cf92bdfd601a0388
ko seon je <kosunje1344@naver.com>
2023-04-09 00:20:01 +0900

    result

--- JavaScript/introduction.js
+++ JavaScript/introduction.js
@@ -2,7 +2,6 @@ const onProfile = () => {
   for (let i = 0; i < targetprofile.length; i++) {
     targetprofile[i].style.visibility = 'visible';
   }
-  console.log(targetprofile.length);
 };
 const onInteresting = (event) => {
   for (let i = 0; i < targetprofile.length; i++) {
