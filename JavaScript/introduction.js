90fa695c8e13d3267ff0c62174ddfab6f1374e87
siifu <kosunje1344@naver.com>
2023-04-11 18:29:08 +0900

    baseball(cow)

---
+++ JavaScript/introduction.js
@@ -0,0 +1,10 @@
+const onProfile = () => {
+  for (let i = 0; i < targetprofile.length; i++) {
+    targetprofile[i].style.visibility = 'visible';
+  }
+};
+const onInteresting = (event) => {
+  for (let i = 0; i < targetprofile.length; i++) {
+    targetinteresting[i].style.visibility = 'visible';
+  }
+};
